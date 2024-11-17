9fdb7d0fe92c73e6c43ef3495d70c2937387fb59
antisedative <antisedativ@mail.ru>
2024-11-17 06:18:51 +0300

    up

--- src/modules/auth/pages/AuthPage.vue
+++ src/modules/auth/pages/AuthPage.vue
@@ -1,20 +1,18 @@
 <template>
 	<div class="container">
 		<div class="wrapper">
-			<div class="content">
-				<div class="form">
-					<form class="form-form" action="">
-						<h1 class="form-title">Добро пожаловать</h1>
-						<p class="form-text">Добро пожаловать! Пожалуйста, введите свои данные</p>
-						<CustomLi label="E-mail" class="form-input" />
-						<CustomLi label="Телефон" class="form-input" />
-						<CustomLi label="ФИО" class="form-input" />
-						<Button class="form-btn" type="submit" priority="find" text="Войти" />
-					</form>
-				</div>
+			<div class="form">
+				<form class="form-form" action="">
+					<h1 class="form-title">Добро пожаловать</h1>
+					<p class="form-text">Добро пожаловать! Пожалуйста, введите свои данные</p>
+					<CustomLi label="E-mail" class="form-input" />
+					<CustomLi label="Телефон" class="form-input" />
+					<CustomLi label="ФИО" class="form-input" />
+					<Button class="form-btn" type="submit" priority="find" text="Войти" />
+				</form>
 			</div>
 			<div class="img">
-				<img src="@/assets/images/cardBack.svg" alt="" />
+				<img src="@/assets/images/test.svg" alt="" />
 			</div>
 		</div>
 	</div>
@@ -26,10 +24,8 @@ import CustomLi from '@modules/core/components/CustomLi.vue';
 </script>
 
 <style scoped lang="scss">
-.content {
-	width: 286px;
-}
 .container {
+	margin-top: 10%;
 	overflow: hidden;
 	text-align: center;
 	display: flex;
@@ -37,43 +33,59 @@ import CustomLi from '@modules/core/components/CustomLi.vue';
 	justify-content: center;
 	align-items: center;
 }
+
 .wrapper {
 	width: 100%;
-	padding-left: 240px;
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+	max-width: 1100px;
+}
+
+.form {
+	width: 310px;
+	text-align: start;
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: flex-start;
+}
+
+.form-form {
+	width: 100%;
+}
+
+.form-title {
+	font-size: 29px;
+	margin-bottom: 10px;
+}
+
+.form-text {
+	width: 100%;
+	margin-bottom: 30px;
+	font-size: 12px;
+}
+
+.form-input {
+	margin-bottom: 20px;
+}
+
+.form-btn {
+	color: #fff;
+	width: 400px;
+	padding: 8.5px 31px;
+}
+
+.img {
+	width: 50%;
+	height: 50vh;
 	display: flex;
 	justify-content: center;
 	align-items: center;
-}
-.form {
-	width: 100%;
-	text-align: start;
-	&-form {
-		display: flex;
-		flex-direction: column;
-	}
-	&-title {
-		font-size: 29px;
-		margin-bottom: 10px;
-	}
-	&-text {
-		margin-bottom: 30px;
-		font-size: 12px;
-	}
-	&-input {
-		margin-bottom: 20px;
-	}
-	&-btn {
-		color: #fff;
-		width: 100%;
-		padding: 8.5px 31px;
-	}
-}
-.img {
-	height: 100vh;
-	width: 100vh;
 	& img {
 		width: 100%;
 		height: 100%;
+		object-fit: contain; /* Сохраняет пропорции изображения */
 	}
 }
 </style>
